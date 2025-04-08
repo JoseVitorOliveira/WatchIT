901208d79737a07e2590064ee113d6bfebfd31b5
Vitor Oliveira <jose.vitor.oliveira.44@gmail.com>
2025-04-07 22:26:24 -0300

    adding new dist

--- src/api/api-config.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-  baseUrl: import.meta.env.VITE_BASE_URL,
-  apiKey: import.meta.env.VITE_API_KEY,
-  largeImage: (imgPath) => `${import.meta.env.VITE_LARGE_IMAGE_URL}/${imgPath}`,
-  w500Image: (imgPath) => `${import.meta.env.VITE_W500_IMAGE_URL}/${imgPath}`,
-};
-
-export default apiConfig;
--- src/pages/ContentPage.jsx
+++
@@ -1,9 +0,0 @@
-import ContentGrid from "../components/content-grid/ContentGrid";
-
-export default function ContentPage({ category }) {
-  return (
-    <main className="bg-dark-gray">
-      <ContentGrid category={category} />
-    </main>
-  );
-}
