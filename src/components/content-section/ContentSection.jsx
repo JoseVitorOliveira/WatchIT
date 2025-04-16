0615c0d3a331b2deb2da63421e7d9b0ba391223c
Vitor Oliveira <jose.vitor.oliveira.44@gmail.com>
2025-04-16 13:02:24 -0300

    Adjust padding in ContentSection

--- src/components/content-section/ContentSection.jsx
+++ src/components/content-section/ContentSection.jsx
@@ -4,7 +4,7 @@ import MovieList from "./ContentList";
 export default function ContentSection({ title, category, type, link }) {
   return (
     <section className="px-5 bg-dark-gray font-bold text-white pb-2">
-      <div className="flex justify-between items-center pb-1 md:pb-2">
+      <div className="flex justify-between items-center pb-2 pt-2 md:pb-4">
         <p className="text-sm md:text-xl lg:text-2xl">{title}</p>
         <Link to={link}>
           <button className="text-[10px] border-white border-2 px-3 py-0.5 rounded-2xl md:text-sm lg:text-lg">
